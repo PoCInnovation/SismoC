c879b981998f7bbaea711bc437c8c2252b1c2599
Léo Dubosclard <leo.dubosclard@epitech.eu>
2022-11-30 22:38:53 +0100

    feature: join group on chain

---
+++ Contract/tasks/deploy.js
@@ -0,0 +1,35 @@
+const { task, types } = require("hardhat/config")
+
+task("deploy", "Deploy a Greeter contract")
+    .addOptionalParam("semaphore", "Semaphore contract address", undefined, types.address)
+    .addParam("group", "Group identifier", 42, types.int)
+    .addOptionalParam("logs", "Print the logs", true, types.boolean)
+    .setAction(async ({ logs, semaphore: semaphoreAddress, group: groupId }, { ethers, run }) => {
+        if (!semaphoreAddress) {
+            const { address: verifierAddress } = await run("deploy:verifier", { logs, merkleTreeDepth: 20 })
+
+            const { address } = await run("deploy:semaphore", {
+                logs,
+                verifiers: [
+                    {
+                        merkleTreeDepth: 20,
+                        contractAddress: verifierAddress
+                    }
+                ]
+            })
+
+            semaphoreAddress = address
+        }
+
+        const Greeter = await ethers.getContractFactory("Greeter")
+
+        const greeter = await Greeter.deploy(semaphoreAddress, groupId)
+
+        await greeter.deployed()
+
+        if (logs) {
+            console.log(`Greeter contract has been deployed to: ${greeter.address}`)
+        }
+
+        return greeter
+    })
